7512524de60e84211469b9f63bd5e60aeebf0340
Riley Shaw <contact@shiney.dev>
2019-04-21 06:05:25 +0100

    add LICENCE

--- games/game.py
+++ games/game.py
@@ -1,12 +1,26 @@
 """
 /game.py
+
+    Copyright (c) 2019 ShineyDev
+    
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
 """
 
-__authors__      = [("shineydev", "contact@shiney.dev")]
-__maintainers__  = [("shineydev", "contact@shiney.dev")]
+__authors__           = [("shineydev", "contact@shiney.dev")]
+__maintainers__       = [("shineydev", "contact@shiney.dev")]
 
-__version_info__ = (1, 0, 0, "stable", 0)
-__version__      = ".".join([str(n) for n in __version_info__[:4:]])
+__version_info__      = (1, 0, 0, "alpha", 0)
+__version__           = "{0}.{1}.{2}{3}{4}".format(*[str(n)[0] if (i == 3) else str(n) for (i, n) in enumerate(__version_info__)])
 
 
 import os
