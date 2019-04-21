432457db10d675a7f22334659effcd8f9185713e
Riley Shaw <contact@shiney.dev>
2019-04-21 06:11:53 +0100

    bump version

--- games/games/uno.py
+++ games/games/uno.py
@@ -19,7 +19,7 @@
 __authors__           = [("shineydev", "contact@shiney.dev")]
 __maintainers__       = [("shineydev", "contact@shiney.dev")]
 
-__version_info__      = (2, 0, 0, "alpha", 0)
+__version_info__      = (2, 0, 0, "final", 0)
 __version__           = "{0}.{1}.{2}{3}{4}".format(*[str(n)[0] if (i == 3) else str(n) for (i, n) in enumerate(__version_info__)])
 
 
